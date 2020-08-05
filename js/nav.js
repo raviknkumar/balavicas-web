af414aa97f5cdffffa1d76e5e38a52d0fc51c03c
iamravi <ravikumar.k@ninjacart.com>
2020-08-06 00:50:05 +0530

    Navbar

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("myNavbar").style.padding = "10px 10px";
+    document.getElementsByClassName("navbar-brand")[0].style.fontSize = "20px";
+    //document.getElementById("myNavbar").style.backgroundColor = "#f8f9fa";
+  } else {
+    document.getElementById("myNavbar").style.padding = "30px 10px";
+    document.getElementsByClassName("navbar-brand")[0].style.fontSize = "35px";
+    //document.getElementById("myNavbar").style.backgroundColor = "transparent";
+  }
+}
