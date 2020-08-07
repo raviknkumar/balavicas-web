8df7598fd0ca070374b4ed5b98af9a2edd8ad13e
iamravi <ravikumar.k@ninjacart.com>
2020-08-08 02:20:51 +0530

    Navbar fix

--- js/nav.js
+++ js/nav.js
@@ -4,10 +4,14 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("myNavbar").style.padding = "10px 10px 2px 10px";
     document.getElementsByClassName("navbar-brand")[0].style.fontSize = "20px";
-    //document.getElementById("myNavbar").style.backgroundColor = "#f8f9fa";
+    document.getElementById("myNavbar").style.backgroundColor = "#232526";
   } else {
     document.getElementById("myNavbar").style.padding = "30px 10px 2px 10px";
     document.getElementsByClassName("navbar-brand")[0].style.fontSize = "35px";
-    //document.getElementById("myNavbar").style.backgroundColor = "transparent";
+    document.getElementById("myNavbar").style.backgroundColor = "transparent";
   }
 }
+
+$('.navbar-toggler').click(function() {
+  $('#myNavbar').toggleClass('nav-open');
+});
