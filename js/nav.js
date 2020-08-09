a399dbf519720684998d6195214ee1dee1d6ebbf
iamravi <ravikumar.k@ninjacart.com>
2020-08-09 11:31:11 +0530

    logo size change added

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,17 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  navicon = $(".navIcon");
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("myNavbar").style.padding = "10px 10px 2px 10px";
     document.getElementsByClassName("navbar-brand")[0].style.fontSize = "20px";
     document.getElementById("myNavbar").style.backgroundColor = "#232526";
+    navicon.attr("width", "45px");
   } else {
     document.getElementById("myNavbar").style.padding = "30px 10px 2px 10px";
     document.getElementsByClassName("navbar-brand")[0].style.fontSize = "35px";
     document.getElementById("myNavbar").style.backgroundColor = "transparent";
+    navicon.attr("width", "75px");
   }
 }
 
